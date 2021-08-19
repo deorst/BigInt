aea57f3f0aac179308193c196bf2d0d35e363e22
Dmitry Stadnik <dstdnky@gmail.com>
2021-08-20 00:17:04 +0500

    Refactor `core` functions

---
+++ src/core.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+
+typedef int T;
+
+namespace BICore
+{
+  std::vector<T> add(std::vector<T> &out, const std::vector<T> &a, const std::vector<T> &b)
+  {
+    if (a.size() >= b.size())
+    {
+      if (a.size() > out.size())
+        out.resize(a.size());
+      int carry{};
+      for (int i{}; i < b.size(); ++i)
+      {
+        out[i] = b[i] + a[i] + carry;
+        carry = out[i] / 10;
+        out[i] %= 10;
+      }
+
+      for (int i{static_cast<int>(b.size())}; i < a.size(); ++i)
+      {
+        out[i] = a[i] + carry;
+        carry = out[i] / 10;
+        out[i] %= 10;
+      }
+      if (carry)
+        out.push_back(carry);
+      return out;
+    }
+    else
+      return add(out, b, a);
+  }
+  bool equal(const std::vector<T> &a, const std::vector<T> &b)
+  {
+    if (a.size() != b.size())
+      return false;
+    else
+    {
+      for (int i{}; i < a.size(); ++i)
+      {
+        if (a[i] != b[i])
+          return false;
+      }
+    }
+    return true;
+  }
+  bool greater(const std::vector<T> &a, const std::vector<T> &b)
+  {
+    if (a.size() != b.size())
+    {
+      return (a.size() > b.size());
+    }
+    else
+    {
+      for (int i{static_cast<int>(a.size()) - 1}; i >= 0; --i)
+      {
+        if (a[i] != b[i])
+          return (a[i] > b[i]);
+      }
+    }
+    return false;
+  }
+}
